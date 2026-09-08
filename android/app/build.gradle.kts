f8c335055875f4fd7a007e767fde57f398fbf8ed
kingofline-web <kingofline@gmail.com>
2026-09-09 01:08:30 +0200

    Set NDK version to 28.2.13676358

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 android {
     namespace = "it.saluterisponde.app"
     compileSdk = 36
+ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
